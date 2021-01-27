065c0276c65b54c4a8b2ea6af31ae153f946c60d
Chris Mitchell <chrism@dropbox.com>
2021-01-26 16:11:41 -0800

    Add scoping-navgraph module with concrete classes

---
+++ scoping-navgraph/src/main/java/com/dropbox/kaiken/scoping_navgraph/AuthAwareNavHostFragment.kt
@@ -0,0 +1,6 @@
+package com.dropbox.kaiken.scoping_navgraph
+
+import androidx.navigation.fragment.NavHostFragment
+import com.dropbox.kaiken.scoping.AuthAwareFragment
+
+class AuthAwareNavHostFragment : NavHostFragment(), AuthAwareFragment
