7d827830f6af8064beb370fece0de65e8a883699
miken <miken@dropbox.com>
2021-12-09 15:01:46 -0500

    few more tests

--- skeleton/src/test/java/com/dropbox/kaiken/skeleton/scoping/SkeletonOwnerTests.kt
+++ skeleton/src/test/java/com/dropbox/kaiken/skeleton/scoping/SkeletonOwnerTests.kt
@@ -1,5 +1,8 @@
 package com.dropbox.kaiken.skeleton.scoping
 
+import com.dropbox.kaiken.skeleton.core.KaikenConfig
+import com.dropbox.kaiken.skeleton.core.SkeletonAccessTokenPair
+import com.dropbox.kaiken.skeleton.core.SkeletonUser
 import com.dropbox.kaiken.skeleton.scoping.fugazi.FakeSkeletonOwner
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
@@ -8,9 +11,22 @@ class SkeletonOwnerTests {
     @Test
     fun testSkeletonOwner() {
         val skeletonServices = DaggerSkeletonTestComponent.factory().create()
-
         val skeleton = FakeSkeletonOwner(skeletonServices).appSkeleton
         val appServices = skeleton.provideAppServices()
+        assertThat(appServices).isInstanceOf(AppComponent::class.java)
+        val userServicesFactory = skeleton.userServicesFactory
+        val userServices: UserServices = userServicesFactory(
+            appServices, SkeletonUser("1", SkeletonAccessTokenPair("1", "1"), isActiveUser = true)
+        )
+        assertThat(userServices).isNotNull()
+    }
+
+    @Test
+    fun testKaikenConfig() {
+        val skeletonServices = DaggerSkeletonTestComponent.factory().create()
+
+        val config = KaikenConfig()
+        val appServices: AppServices = config.scopedServicesFactory.createAppServices(skeletonServices)
         assertThat(appServices).isInstanceOf(AppComponent::class.java)
     }
 }
