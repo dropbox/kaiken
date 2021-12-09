7e6b908ee15d0cec1392eda7a9196fd974289228
miken <miken@dropbox.com>
2021-12-09 12:22:28 -0500

    lint

--- skeleton/src/main/java/com/dropbox/kaiken/skeleton/core/SkeletonOwner.kt
+++ skeleton/src/main/java/com/dropbox/kaiken/skeleton/core/SkeletonOwner.kt
@@ -1,12 +1,40 @@
 package com.dropbox.kaiken.skeleton.core
 
-import android.app.Application
+import com.dropbox.kaiken.skeleton.dagger.SdkSpec
+import com.dropbox.kaiken.skeleton.dependencymanagement.SkeletonScopedServices
+import com.dropbox.kaiken.skeleton.scoping.AppServices
+import com.dropbox.kaiken.skeleton.scoping.UserServices
+import com.dropbox.kaiken.skeleton.scoping.UserServicesProvider
 
-abstract class SkeletonOwnerApplication : SkeletonOwner, Application() {
-    override fun onCreate() {
-        super.onCreate()
+interface SkeletonOwner : SkeletonScopedServices {
+
+    var appSkeleton: AppSkeletonDelegate
+    override val component: SdkSpec
+        get() = appSkeleton.component
+
+    fun getSdkSpec(): SdkSpec
+    override fun provideAppServices() = appSkeleton.provideAppServices()
+    override val appServices: AppServices
+        get() = provideAppServices()
+
+    override fun provideUserServicesOf(userId: String) = appSkeleton.provideUserServicesOf(userId)
+    override val userServicesFactory: (AppServices, SkeletonUser) -> UserServices
+        get() = appSkeleton.userServicesFactory
+
+    override var userServicesProvider: UserServicesProvider
+        get() = appSkeleton.userServicesProvider
+        set(value) {
+            appSkeleton.userServicesProvider = value
+        }
+}
+
+class FakeSkeletonOwner : SkeletonOwner {
+    override lateinit var appSkeleton: AppSkeletonDelegate
+    lateinit var fakeSdkSpec: SdkSpec
+    override fun getSdkSpec(): SdkSpec =
+        fakeSdkSpec
+
+    fun initialize() {
         appSkeleton = AppSkeletonInitializer.init(getSdkSpec()).appSkeletonDelegate
     }
-
-    override lateinit var appSkeleton: AppSkeletonDelegate
 }
