0b5468bba754c26cbe5a909c9f1d3e71642ebab0
Chris Mitchell <chrism@dropbox.com>
2021-12-15 16:18:35 -0800

    fix more a lint

--- skeleton/src/test/java/com/dropbox/kaiken/skeleton/scoping/SkeletonTestComponent.kt
+++ skeleton/src/test/java/com/dropbox/kaiken/skeleton/scoping/SkeletonTestComponent.kt
@@ -2,7 +2,6 @@ package com.dropbox.kaiken.skeleton.scoping
 
 import com.dropbox.kaiken.skeleton.core.SkeletonUser
 import com.dropbox.kaiken.skeleton.dagger.SdkSpec
-import com.dropbox.kaiken.skeleton.usermanagement.SkeletonMapper
 import com.squareup.anvil.annotations.ContributesTo
 import com.squareup.anvil.annotations.MergeComponent
 import dagger.Component
@@ -10,7 +9,6 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 
 @MergeComponent(SkeletonScope::class)
 @SingleIn(SkeletonScope::class)
