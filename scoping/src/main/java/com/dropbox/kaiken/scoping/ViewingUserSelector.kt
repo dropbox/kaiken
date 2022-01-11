e748e3648c93f13cabf7743146a40beca0ecd66a
Chris Mitchell <chrism@dropbox.com>
2022-01-11 13:47:01 -0800

    Update to latest parcelize (#114)
    * Update to latest kotlin parcelize plugin
    
    * Fix lint errors
    
    * fix viewbinding errors
    
    * Remove unnecessary plugin
    
    * fix thing

--- scoping/src/main/java/com/dropbox/kaiken/scoping/ViewingUserSelector.kt
+++ scoping/src/main/java/com/dropbox/kaiken/scoping/ViewingUserSelector.kt
@@ -5,7 +5,7 @@ package com.dropbox.kaiken.scoping
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 private const val BUNDLE_KEY =
     "com.dropbox.kaiken.skeleton.scoping.VIEWING_USER_SELECTOR_BUNDLE_KEY"
@@ -15,6 +15,7 @@ private const val BUNDLE_KEY =
  * fragment.
  */
 @Parcelize
+@SuppressWarnings("ParcelCreator") // https://youtrack.jetbrains.com/issue/KT-19300
 data class ViewingUserSelector(internal val userId: String) : Parcelable {
     companion object {
         @JvmStatic
