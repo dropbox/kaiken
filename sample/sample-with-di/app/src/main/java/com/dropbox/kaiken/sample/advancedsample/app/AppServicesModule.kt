de86980471bf2e325a8f93d05d856fea95a06e2f
Chris Mitchell <chrism@dropbox.com>
2022-02-28 09:14:02 -0800

    Fix documentation for app and user initializers (#156)
    * Fix documentation for app and user initializers
    
    * fix

--- sample/sample-with-di/app/src/main/java/com/dropbox/kaiken/sample/advancedsample/app/AppServicesModule.kt
+++ sample/sample-with-di/app/src/main/java/com/dropbox/kaiken/sample/advancedsample/app/AppServicesModule.kt
@@ -1,14 +1,18 @@
 package com.dropbox.kaiken.sample.advancedsample.app
 
+import android.util.Log
 import com.dropbox.common.inject.AppScope
 import com.dropbox.kaiken.sample.advancedsample.helloworldfeature.HelloWorldMessageProvider
 import com.dropbox.kaiken.sample.advancedsample.helloworldfeature.RealTimeMessageProvider
 import com.dropbox.kaiken.sample.advancedsample.helloworldfeature.RealWorldMessageProvider
 import com.dropbox.kaiken.sample.advancedsample.helloworldfeature.TimeMessageProvider
+import com.dropbox.kaiken.skeleton.initializers.AppServicesInitializer
 import com.dropbox.kaiken.skeleton.scoping.SingleIn
+import com.squareup.anvil.annotations.ContributesMultibinding
 import com.squareup.anvil.annotations.ContributesTo
 import dagger.Module
 import dagger.Provides
+import javax.inject.Inject
 
 @Module
 @ContributesTo(AppScope::class)
@@ -21,4 +25,11 @@ object AppServicesModule {
     @Provides
     @SingleIn(AppScope::class)
     fun provideTimeMessageProvider(): TimeMessageProvider = RealTimeMessageProvider()
+}
+
+@ContributesMultibinding(AppScope::class)
+class HelloWorldAppServicesInitializer @Inject constructor() : AppServicesInitializer {
+    override fun init() {
+        Log.d("TAG", "App Services initializing for $this")
+    }
 }
--- skeleton/src/main/java/com/dropbox/kaiken/skeleton/initializers/UserServicesInitializer.kt
+++ skeleton/src/main/java/com/dropbox/kaiken/skeleton/initializers/UserServicesInitializer.kt
@@ -10,8 +10,7 @@ import dagger.multibindings.ElementsIntoSet
  * Interface to use when you have a component which you want to be called
  * when a user services is created.  Be mindful this will be run on the main thread.
  *
- * @ContributesMultibinding(SdkScope::class)
- * @SingleIn(SdkScope::class)
+ * @ContributesMultibinding(UserScope::class)
  * class FooInitializer @Inject constructor(
  *     private val foo: Foo,
  *     private val bar: Bar,
