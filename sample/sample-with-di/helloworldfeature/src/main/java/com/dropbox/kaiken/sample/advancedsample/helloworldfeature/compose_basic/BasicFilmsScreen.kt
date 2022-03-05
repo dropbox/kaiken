a66e3b1c2df07b7ca760c9539d46ff84481dadca
Ken Shimizu <kshimizu@dropbox.com>
2022-03-04 16:39:20 -1000

    Compose Kaiken 3.4

--- sample/sample-with-di/helloworldfeature/src/main/java/com/dropbox/kaiken/sample/advancedsample/helloworldfeature/compose_basic/BasicFilmsScreen.kt
+++ sample/sample-with-di/helloworldfeature/src/main/java/com/dropbox/kaiken/sample/advancedsample/helloworldfeature/compose_basic/BasicFilmsScreen.kt
@@ -1,8 +1,19 @@
 package com.dropbox.kaiken.sample.advancedsample.helloworldfeature.compose_basic
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dropbox.kaiken.sample.advancedsample.helloworldfeature.authed_example.repository.Film
 
 @Composable
 fun BasicFilmsScreen(
@@ -13,6 +24,32 @@ fun BasicFilmsScreen(
         CircularProgressIndicator()
         handleEvent(BasicFilmsPresenter.LoadFilms)
     } else {
-        Text(text = "has films")
+        FilmList(model.films)
+    }
+}
+
+@Composable
+fun FilmList(films: List<Film>) {
+    LazyColumn {
+        films.forEach {
+            item { FilmRow(it) }
+        }
+    }
+}
+
+@Composable
+fun FilmRow(film: Film) {
+    Column(Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(12.dp)) {
+        Text(
+            text = "${film.releaseDate} - ${film.title}",
+            style = MaterialTheme.typography.h5
+        )
+        Text(
+            text = film.description,
+            style = MaterialTheme.typography.body1,
+        )
     }
 }
