ce57590c77ce5f8fa7e1edfc6cf5ca6d42e4d9bb
Chris Mitchell <chrism@dropbox.com>
2022-03-24 19:52:31 -0700

    Bind SkeletonUser instead of UserId with Anvil Components

--- skeleton-components/src/test/java/com/dropbox/kaiken/skeleton/scoping/fugazi/FakeSkeletonConfig.kt
+++ skeleton-components/src/test/java/com/dropbox/kaiken/skeleton/scoping/fugazi/FakeSkeletonConfig.kt
@@ -6,17 +6,17 @@ import com.dropbox.kaiken.scoping.UserServices
 import com.dropbox.kaiken.skeleton.components.scoping.AppComponent
 import com.dropbox.kaiken.skeleton.components.scoping.UserParentComponent
 import com.dropbox.kaiken.skeleton.core.AppSpecificScopedServicesFactory
-import com.dropbox.kaiken.skeleton.core.KaikenConfigBinding
+import com.dropbox.kaiken.skeleton.core.KaikenConfig
 import com.dropbox.kaiken.skeleton.core.SkeletonConfig
 import com.dropbox.kaiken.skeleton.core.SkeletonUser
 import com.dropbox.kaiken.skeleton.dagger.SdkSpec
 import com.dropbox.kaiken.skeleton.scoping.SingleIn
 import com.dropbox.kaiken.skeleton.scoping.cast
-import com.squareup.anvil.annotations.ContributesTo
-import dagger.Binds
-import dagger.Module
+import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 
+@ContributesBinding(SkeletonScope::class, replaces = [KaikenConfig::class])
+@SingleIn(SkeletonScope::class)
 class FakeSkeletonConfig @Inject constructor() : SkeletonConfig {
     override val scopedServicesFactory: AppSpecificScopedServicesFactory
         get() = object : AppSpecificScopedServicesFactory {
@@ -32,14 +32,6 @@ class FakeSkeletonConfig @Inject constructor() : SkeletonConfig {
                 appServices
                     .cast<UserParentComponent>()
                     .createUserComponent()
-                    .userComponent(user.userId.toInt())
+                    .userComponent(user)
         }
-}
-
-@ContributesTo(SkeletonScope::class, replaces = [KaikenConfigBinding::class])
-@Module
-abstract class FakeSkeletonConfigBinding {
-    @SingleIn(SkeletonScope::class)
-    @Binds
-    abstract fun bindSkeletonConfig(real: FakeSkeletonConfig): SkeletonConfig
 }
